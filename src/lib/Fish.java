8c3ed0cd9ee21036a19937c4e74e9497464d9f86
manasye <manasyebukit@gmail.com>
2018-04-17 10:22:24 +0700

    Implement good checkstyle in Fish
    One checkstyle problem still found : missing package-info

--- src/lib/Fish.java
+++ src/lib/Fish.java
@@ -1,19 +1,62 @@
 import java.util.Random;
 
+/**
+ * The type Fish.
+ */
 public class Fish extends AqObject implements Moveable {
-    private static final int INIT_HUNGRY_TIME = 15;
-    private static final int INIT_DYING_TIME = 20;
-    private static final int QUARTER_DEGREE = 90;
-    private static final int THREE_QUART_DEGREE = 270;
-    private static final int FULL_QUART_DEGREE = 360;
-    private int numEaten; // Fish has eaten how many times
-    private char facing;
-    private boolean fishFull; // Guppy is full or not
-    private int degOfMovement; // Stores degree of movement [0..359]
-    private double timeBeforeHungry, timeBeforeDying, timeToRandomize; // Timers
-    private double toX, toY; // If this is set, Fish will move towards X and Y
 
-    // Constructor
+    /**
+     * Constants for Initial Hungry Time.
+     */
+    private static final int INIT_HUNGRY_TIME = 15;
+    /**
+     * Constants for Initial Dying Time.
+     */
+    private static final int INIT_DYING_TIME = 20;
+    /**
+     * Constants for 90 degrees.
+     */
+    private static final int QUARTER_DEGREE = 90;
+    /**
+     * Constants for 270 degrees.
+     */
+    private static final int THREE_QUART_DEGREE = 270;
+    /**
+     * Constants for 360 degrees.
+     */
+    private static final int FULL_QUART_DEGREE = 360;
+    /**
+     * Variables describing fish have eaten how many times.
+     */
+    private int numEaten;
+    /**
+     * Variables describing face of the fish.
+     */
+    private char facing;
+    /**
+     * Variables describing is fish in a non-hungry state.
+     */
+    private boolean fishFull;
+    /**
+     * Variables describing degree of movement of fish.
+     */
+    private int degOfMovement;
+    /**
+     * Variables describing time needed for move and hunger mechanism.
+     */
+    private double timeBeforeHungry, timeBeforeDying, timeToRandomize;
+    /**
+     * Variables describing fish go to what coordinates.
+     */
+    private double toX, toY;
+
+    /**
+     * Instantiates a new Fish.
+     *
+     * @param x the x
+     * @param y the y
+     * @param z the z
+     */
     public Fish(final double x, final double y, final double z) {
         super(x, y, z);
         numEaten = 0;
@@ -27,86 +70,184 @@ public class Fish extends AqObject implements Moveable {
         toY = -1;
     }
 
-    // Getter
+    /**
+     * Gets num eaten.
+     *
+     * @return the num eaten
+     */
     public int getNumEaten() {
         return numEaten;
     }
 
+    /**
+     * Gets facing.
+     *
+     * @return the facing
+     */
     public char getFacing() {
         return facing;
     }
 
+    /**
+     * Is fish full boolean.
+     *
+     * @return the boolean
+     */
     public boolean isFishFull() {
         return fishFull;
     }
 
+    /**
+     * Gets deg of movement.
+     *
+     * @return the deg of movement
+     */
     public int getDegOfMovement() {
         return degOfMovement;
     }
 
+    /**
+     * Gets time before hungry.
+     *
+     * @return the time before hungry
+     */
     public double getTimeBeforeHungry() {
         return timeBeforeHungry;
     }
 
+    /**
+     * Gets time before dying.
+     *
+     * @return the time before dying
+     */
     public double getTimeBeforeDying() {
         return timeBeforeDying;
     }
 
+    /**
+     * Gets to x.
+     *
+     * @return the to x
+     */
     public double getToX() {
         return toX;
     }
 
+    /**
+     * Gets to y.
+     *
+     * @return the to y
+     */
     public double getToY() {
         return toY;
     }
 
-    // Setter
+    /**
+     * Sets num eaten.
+     *
+     * @param numOfEaten the num of eaten
+     */
+
     public void setNumEaten(final int numOfEaten) {
         this.numEaten = numOfEaten;
     }
 
+    /**
+     * Sets facing.
+     *
+     * @param face the face
+     */
     public void setFacing(final char face) {
         this.facing = face;
     }
 
+    /**
+     * Sets fish full.
+     *
+     * @param full the full
+     */
     public void setFishFull(final boolean full) {
         this.fishFull = full;
     }
 
+    /**
+     * Sets deg of movement.
+     *
+     * @param degree the degree
+     */
     public void setDegOfMovement(final int degree) {
         this.degOfMovement = degree;
     }
 
+    /**
+     * Sets time before hungry.
+     *
+     * @param time the time
+     */
     public void setTimeBeforeHungry(final double time) {
         this.timeBeforeHungry = time;
     }
 
+    /**
+     * Sets time before dying.
+     *
+     * @param time the time
+     */
     public void setTimeBeforeDying(final double time) {
         this.timeBeforeDying = time;
     }
 
+    /**
+     * Sets time to randomize.
+     *
+     * @param time the time
+     */
     public void setTimeToRandomize(final double time) {
         this.timeToRandomize = time;
     }
 
+    /**
+     * Sets to x.
+     *
+     * @param x the x
+     */
     public void setToX(final double x) {
         this.toX = x;
     }
 
+    /**
+     * Sets to y.
+     *
+     * @param y the y
+     */
     public void setToY(final double y) {
         this.toY = y;
     }
 
+    /**
+     * Move towards.
+     *
+     * @param x the x
+     * @param y the y
+     */
     public void moveTowards(final double x, final double y) {
         setToX(x);
         setToY(y);
     }
 
+    /**
+     * Move randomly.
+     */
     public void moveRandomly() {
         setToX(-1);
         setToY(-1);
     }
 
+    /**
+     * Countdown hungry.
+     *
+     * @param dtime the dtime
+     */
     public void countdownHungry(final double dtime) {
         if (timeBeforeHungry <= 0) {
             fishFull = false;
@@ -115,6 +256,11 @@ public class Fish extends AqObject implements Moveable {
         }
     }
 
+    /**
+     * Countdown dying.
+     *
+     * @param dtime the dtime
+     */
     public void countdownDying(final double dtime) {
         if (timeBeforeDying <= 0) {
             setIsAlive(false); // Kill the fish
@@ -123,6 +269,9 @@ public class Fish extends AqObject implements Moveable {
         }
     }
 
+    /**
+     * @param timePassed refers to time already passed
+     */
     public void move(final double timePassed) {
         double curDegOfMovement;
         if ((toX != -1) && !fishFull) {
@@ -157,6 +306,10 @@ public class Fish extends AqObject implements Moveable {
         setY(getY() + getSpeed() * Math.sin(curDegOfMovement) * timePassed);
     }
 
+
+    /**
+     * @param dtime refers to time that already passed
+     */
     public void timeHasPassed(final double dtime) {
         this.move(dtime);
         if (fishFull) {
