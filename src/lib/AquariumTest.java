447b53567a18000730ea8e08380253b304fa7356
Antonio Setya <antonio_setya@hotmail.com>
2018-04-23 19:56:46 +0700

    Additions to AquariumTest
    * Added two more testcase
    * Added description of testcase

--- src/lib/AquariumTest.java
+++ src/lib/AquariumTest.java
@@ -12,12 +12,16 @@ public class AquariumTest {
 	}
 	
 	@Test
+	/* Used to test default ctor */
 	public void testDefaultCtor() {
 		assertEquals(test.getWidth(),640);
 		assertEquals(test.getHeight(),480);
 	}
 
 	@Test
+	/* Used to test alternate ctor
+	 * (the one with parameters on it)
+	 */
 	public void testAlternateCtor() {
 		test = new Aquarium(1000,1000);
 		assertEquals(test.getWidth(),1000);
@@ -25,6 +29,7 @@ public class AquariumTest {
 	}
 	
 	@Test
+	/* Used to test getters & setters for width & height */
 	public void testDimChange() {
 		test.setWidth(250);
 		assertEquals(test.getWidth(),250);
@@ -33,6 +38,7 @@ public class AquariumTest {
 	}
 	
 	@Test
+	/* Used to test getters & setters for numOfCoins */
 	public void testNumOfCoin() {
 		assertEquals(test.getNumOfCoins(),150);
 		test.setNumOfCoins(test.getNumOfCoins() + 200);
@@ -40,6 +46,7 @@ public class AquariumTest {
 	}
 	
 	@Test
+	/* Used to test method initialize() */
 	public void testInitialize() {
 		test.initialize();
 		assertEquals(test.getGuppies().getSize(),1);
@@ -53,6 +60,7 @@ public class AquariumTest {
 	}
 	
 	@Test
+	/* Used to test method createNewObject() */
 	public void testObjectCreation() {
 		test.initialize();
 		test.createNewObject('G');
@@ -69,6 +77,7 @@ public class AquariumTest {
 	}
 	
 	@Test
+	/* Used to test method keepOnAquarium() */
 	public void testKeepOnAquarium() {
 		test.createNewObject('G',250,300);
 		test.keepOnAquarium(test.getGuppies().get(0));
@@ -89,10 +98,13 @@ public class AquariumTest {
 	}
 	
 	@Test
+	/* Used to test that a Piranha will eat nearby Guppy only if it's hungry */
 	public void testTimeHasPassedEaten1() {
 		test.createNewObject('G',175,175);
-		test.createNewObject('P',176,176);
+		test.createNewObject('P',175,175);
 		test.setSnail(new Siput(160,test.getHeight()));
+		test.timeHasPassed(0.033);
+		assertEquals(test.getGuppies().getSize(),1);
 		test.getPiranhas().get(0).setFishFull(false);
 		test.timeHasPassed(0.033);
 		assertEquals(test.getGuppies().getSize(),0);
@@ -101,17 +113,21 @@ public class AquariumTest {
 	}
 	
 	@Test
+	/* Used to test that a Guppy will eat nearby FishFood only if it's hungry */
 	public void testTimeHasPassedEaten2() {
 		test.createNewObject('G',175,175);
-		test.getGuppies().get(0).setFishFull(false);
 		test.setSnail(new Siput(160,test.getHeight()));
-		test.createNewObject('F',176,176);
+		test.createNewObject('F',175,175);
+		test.timeHasPassed(0.033);
+		assertEquals(test.getFishFoods().getSize(),1);
+		test.getGuppies().get(0).setFishFull(false);
 		test.timeHasPassed(0.033);
 		assertEquals(test.getFishFoods().getSize(),0);
 		assertTrue(test.getGuppies().get(0).isFishFull());
 	}
 	
 	@Test
+	/* Used to test if Snail will pick up nearby Coins */
 	public void testTimeHasPassedEaten3() {
 		long initial = test.getNumOfCoins();
 		test.getCoins().append(new Coin(162,test.getHeight(),20));
@@ -122,6 +138,34 @@ public class AquariumTest {
 	}
 	
 	@Test
+	/* Used to test that a Piranha will only eat 1 Guppy when hungry */
+	public void testTimeHasPassedEaten4() {
+		test.createNewObject('G',175,175);
+		test.createNewObject('G',177,177);
+		test.createNewObject('P',176,176);
+		test.setSnail(new Siput(160,test.getHeight()));
+		test.getPiranhas().get(0).setFishFull(false);
+		test.timeHasPassed(0.033);
+		assertEquals(test.getGuppies().getSize(),1);
+		assertTrue(test.getPiranhas().get(0).isFishFull());
+		assertEquals(test.getCoins().getSize(),1);
+	}
+	
+	@Test
+	/* Used to test that a Guppy will only eat 1 FishFood when hungry */
+	public void testTimeHasPassedEaten5() {
+		test.createNewObject('G',175,175);
+		test.getGuppies().get(0).setFishFull(false);
+		test.setSnail(new Siput(160,test.getHeight()));
+		test.createNewObject('F',176,176);
+		test.createNewObject('F',177,177);
+		test.timeHasPassed(0.033);
+		assertEquals(test.getFishFoods().getSize(),1);
+		assertTrue(test.getGuppies().get(0).isFishFull());
+	}
+	
+	@Test
+	/* Used to test method CollectCoin() */
 	public void testCollectCoin() {
 		long initial = test.getNumOfCoins();
 		test.getCoins().append(new Coin(150,250,30));
